af197637c968416d14e2ee321e7ba46a9198e1f0
ymjn221 <gerel0899@gmail.com>
2026-04-08 00:03:55 +0800

    Change animal names to English in ZooApp

--- src/ZooApp.java
+++ src/ZooApp.java
@@ -4,9 +4,9 @@ import java.util.List;
 public class ZooApp {
 	public static void main(String[] args) {
 		Zoo<Mammal> mammalZoo = new Zoo<>();
-		mammalZoo.addAnimal(new Mammal("Арслан"));
+		mammalZoo.addAnimal(new Mammal("Arslan"));
 		Zoo<Bird> birdZoo = new Zoo<>();
-		birdZoo.addAnimal(new Bird("Тогоруу"));
+		birdZoo.addAnimal(new Bird("Togoruu"));
 		List<Animal> generalZoo = new ArrayList<>();
 		mammalZoo.transferAnimals(mammalZoo.getAnimals(), generalZoo);
 		birdZoo.transferAnimals(birdZoo.getAnimals(), generalZoo);

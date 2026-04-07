4d8bf4b600814db42a876ba3dc238bcbca676eb9
ymjn221 <gerel0899@gmail.com>
2026-04-08 00:03:03 +0800

    Change comments to English in StringProcessor.java
    Updated the comments in the processStrings method and main method to use English terms.

--- src/StringProcessor.java
+++ src/StringProcessor.java
@@ -6,11 +6,11 @@ public class StringProcessor {
 			Predicate<String> filter,
 			Function<String, String> transformer,
 			Consumer<String> printer) {
-		// Хэрэгжүүл: filter, transform, print хий
+	
 	}
 
 	public static void main(String[] args) {
-		List<String> words = Arrays.asList("Java", "Програмчлал", "Код");
-		// Хэрэгжүүл: Predicate, Function, Consumer-ийг тодорхойлж, processStrings дууд
+		List<String> words = Arrays.asList("Java", "Programchlal", "Code");
+
 	}
 }

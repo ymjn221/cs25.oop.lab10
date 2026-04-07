a289ad40e0315eba729c4cf4ae7c4ba352dfdfeb
ymjn221 <gerel0899@gmail.com>
2026-04-07 23:59:46 +0800

    Update book titles to use English names

--- src/LibApp.java
+++ src/LibApp.java
@@ -1,9 +1,9 @@
 public class LibApp {
 	public static void main(String[] args) {
 		Library lib = new Library();
-		lib.addBook("Гэдэс", 1921);
+		lib.addBook("Gedes", 1921);
 		lib.addBook("1984", 1949);
-		lib.addBook("Сүүн Зам", 2020);
+		lib.addBook("Suun Zam", 2020);
 		for (Book b : lib.getRecentBooks(2000)) {
 			System.out.println(b.getTitle() + ": " + b.getYear());
 		}
